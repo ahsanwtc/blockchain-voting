6237d5399a595ff3f88623790cd0f610235c281d
jsan <ahsan.wtc@gmail.com>
2022-03-29 15:39:21 +0500

    completing tests

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,58 +2,59 @@
 pragma solidity ^0.8.1;
 
 contract Voting {
-    address public admin;
-    mapping(address => bool) public voters;    
-    struct Choice {
-        uint id;
-        string name;
-        uint votes;
-    }
-    struct Ballot {
-        uint id;
-        string name;
-        Choice[] choices;
-        uint end;
-    }
-    mapping(uint => Ballot) public ballots;
-    uint nextBallotId;
-    mapping(address => mapping(uint => bool)) public votes;
+  address public admin;
+  mapping(address => bool) public voters;    
+  struct Choice {
+    uint id;
+    string name;
+    uint votes;
+  }
+  struct Ballot {
+    uint id;
+    string name;
+    Choice[] choices;
+    uint end;
+  }
+  mapping(uint => Ballot) public ballots;
+  uint public nextBallotId;
+  mapping(address => mapping(uint => bool)) public votes;
 
-    constructor() {
-        admin = msg.sender;
-    }
+  constructor() {
+    admin = msg.sender;
+  }
 
-    function addVoters(address[] calldata _voters) external onlyAdmin() {
-        for (uint i = 0; i < _voters.length; i++) {
-            voters[_voters[i]] = true;
-        }
+  function addVoters(address[] calldata _voters) external onlyAdmin() {
+    for (uint i = 0; i < _voters.length; i++) {
+      voters[_voters[i]] = true;
     }
+  }
 
-    function createBallot(string calldata name, string[] calldata choices, uint offset) external onlyAdmin() {
-        ballots[nextBallotId].id = nextBallotId;
-        ballots[nextBallotId].name = name;
-        ballots[nextBallotId].end = block.timestamp + offset;
-        for (uint i = 0; i < choices.length; i++) {
-            ballots[nextBallotId].choices.push(Choice(i, choices[i], 0));
-        }
+  function createBallot(string calldata name, string[] calldata choices, uint offset) external onlyAdmin() {
+    ballots[nextBallotId].id = nextBallotId;
+    ballots[nextBallotId].name = name;
+    ballots[nextBallotId].end = block.timestamp + offset;
+    for (uint i = 0; i < choices.length; i++) {
+      ballots[nextBallotId].choices.push(Choice(i, choices[i], 0));
     }
+    nextBallotId++;
+  }
 
-    function vote(uint ballotId, uint choiceId) external {
-        require(voters[msg.sender] == true, "only voters");
-        require(votes[msg.sender][ballotId] == false, "voter has already casted it's vote");
-        require(block.timestamp < ballots[ballotId].end, "ballot has already ended");
-        votes[msg.sender][ballotId] = true;
-        ballots[ballotId].choices[choiceId].votes++;
-    }
+  function vote(uint ballotId, uint choiceId) external {
+    require(voters[msg.sender] == true, "only voters");
+    require(votes[msg.sender][ballotId] == false, "voter has already casted it's vote");
+    require(block.timestamp < ballots[ballotId].end, "ballot has already ended");
+    votes[msg.sender][ballotId] = true;
+    ballots[ballotId].choices[choiceId].votes++;
+  }
 
-    function results(uint ballotId) view external returns(Choice[] memory) {
-        require(block.timestamp >= ballots[ballotId].end, "ballot is still active");
-        return ballots[ballotId].choices;
-    }
+  function results(uint ballotId) view external returns(Choice[] memory) {
+    require(block.timestamp >= ballots[ballotId].end, "ballot is still active");
+    return ballots[ballotId].choices;
+  }
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "only admin");
-        _;
-    }
+  modifier onlyAdmin() {
+    require(msg.sender == admin, "only admin");
+    _;
+  }
 
 }
--- test/voting.js
+++ test/voting.js
@@ -1,4 +1,4 @@
-const { expectRevert } = require('@openzeppelin/test-helpers');
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
 
 const Voting = artifacts.require("Voting");
 
@@ -25,6 +25,63 @@ contract("Voting", function (accounts) {
     await expectRevert(
       voting.addVoters([accounts[5]], { from: voter1 }),
       'only admin'
+    );
+  });
+
+  it('should create a ballot', async () => {
+    await voting.createBallot('Ballot 1', ['choice 1', 'choice 2', 'choice 3'], 5, { from: admin });
+    const ballot = await voting.ballots(0);
+    await assert(ballot.name === 'Ballot 1');
+  });
+
+  it('should NOT create a ballot', async () => {
+    await expectRevert(
+      voting.createBallot('Ballot 1', ['choice 1', 'choice 2', 'choice 3'], 5, { from: voter2 }),
+      'only admin'
+    );
+  });
+
+  it('should vote', async () => {
+    await voting.createBallot('Ballot 2', ['choice 1', 'choice 2', 'choice 3'], 5, { from: admin });
+    const ballotId = await voting.nextBallotId() - 1;
+
+    await voting.vote(ballotId, 0, { from: voter1 });
+    await voting.vote(ballotId, 2, { from: voter2 });
+    await voting.vote(ballotId, 2, { from: voter3 });
+    await time.increase(5001);
+
+    
+    /* (id, choice, votes), (id, choice, votes), (id, choice, votes) */
+    const results = await voting.results(ballotId);
+    await assert(results[0].votes === '1');
+    await assert(results[1].votes === '0');
+    await assert(results[2].votes === '2');
+  });
+  
+  it('should NOT vote if voter is a non voter', async () => {
+    await voting.createBallot('Ballot 2', ['choice 1', 'choice 2', 'choice 3'], 5, { from: admin });
+    await expectRevert(
+      voting.vote(1, 0, { from: nonVoter }),
+      'only voters'
+    );
+  });
+
+  it('should NOT vote if voter already voted', async () => {
+    await voting.createBallot('Ballot 3', ['choice 1', 'choice 2', 'choice 3'], 5, { from: admin });
+    const ballotId = await voting.nextBallotId() - 1;
+    await voting.vote(ballotId, 0, { from: voter1 });
+    await expectRevert(
+      voting.vote(ballotId, 0, { from: voter1 }),
+      "voter has already casted it's vote"
+    );
+  });
+
+  it('should NOT vote if ballot has ended', async () => {
+    await voting.createBallot('Ballot 4', ['choice 1', 'choice 2', 'choice 3'], 5, { from: admin });
+    await time.increase(5001);
+    await expectRevert(
+      voting.vote(2, 0, { from: voter1 }),
+      'ballot has already ended'
     );
   });
 
